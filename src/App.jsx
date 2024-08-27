f82482a8808f7a6075d970c32ffab97ca1cb05bb
Satyam Thakur <sat.thakur20@gmail.com>
2024-08-27 21:48:58 +0530

    Creating counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { increment, decrement } from "./redux/slices/counter";
+
 function App() {
+  const count = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <>
-      <div>Hello, World</div>
+      <div className="ui vertical masthead center aligned segment">
+        <h1>Count is {count}</h1>
+        <button
+          onClick={() => dispatch(increment())}
+          className="green ui button"
+        >
+          Increment
+        </button>
+        <button onClick={() => dispatch(decrement())} className="red ui button">
+          Decrement
+        </button>
+      </div>
     </>
   );
 }
